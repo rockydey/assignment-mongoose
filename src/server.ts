77f816abd49285af5e82cc95316f5101216aba27
rockydey <rockydey077@gmail.com>
2025-03-08 19:08:34 +0600

    connect mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
-import app from "./app";
-import mongoose from "mongoose";
-import config from "./app/config";
-
-main().catch((err) => console.log(err));
+import app from './app';
+import mongoose from 'mongoose';
+import config from './app/config';
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  try {
+    await mongoose.connect(config.database_url as string);
 
-  app.listen(config.port, () => {
-    console.log(`Express app listening on port ${config.port}`);
-  });
+    app.listen(config.port, () => {
+      console.log(`Express app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 main();
